5df284dec8d024c9f0d1808283f3ca25d355ca89
Issblann <anaisabelpp@outlook.com>
2024-08-28 19:23:00 -0500

    first commit

---
+++ stop-game/src/components/Game/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Row from '../Core/Row';
+import Column from '../Core/Column';
+
+export const Game = () => {
+  return (
+    <div>
+      <Column />
+      <Row />
+    </div>
+  );
+};
---
+++ stop-game/src/constants.js
@@ -0,0 +1,7 @@
+const COLUMNS = [
+  { id: 1, name: 'Letra' },
+  { id: 2, name: 'Nombre' },
+  { id: 3, name: 'Animal' },
+  { id: 4, name: 'Color' },
+  { id: 5, name: 'País' },
+];
